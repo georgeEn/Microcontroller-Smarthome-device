4d36af8c785265a2666298aa30f333fd8a80d414
georgeEn <74372044+georgeEn@users.noreply.github.com>
2021-01-18 12:03:19 +0100

    Add files via upload

---
+++ SMART HOME/HEATING/State Machine/Mobile phone H.c
@@ -0,0 +1,12 @@
+#ifndef _MOBILE PHONE_H
+#define _MOBILE PHONE_H
+
+#define active 1 //active
+#define inative 0 //inactive
+
+void GetroomTemperature();
+void onHeater();
+void offHeater();
+
+
+#endif
